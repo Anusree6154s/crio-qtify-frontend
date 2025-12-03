e7e0167675050f31e0f2824c428cd6986d6f3d40
Anusree <anilkumaranusree113@gmail.com>
2025-12-03 23:27:30 +0530

    fix: make changes to imports to run on dev

--- qtify/api/faq.js
+++ qtify/api/faq.js
@@ -1,5 +1,10 @@
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// ESM-safe __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default function handler(req, res) {
   const filePath = path.join(__dirname, "data", "db.json");
